acf686699d44297de99c9d09f13069f5d49d345b
Esraa <112142189+Esraamurry@users.noreply.github.com>
2023-07-30 14:01:00 -0500

    activity 3

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import connectDatabase from './config/db';
 
-//Intitialize express application
+//Initialize express application
 const app = express();
 
-//Connect database
 connectDatabase();
 
 //API endpoints
-app.get('/', (req, res) =>
+app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
 
-//Connection listener
-app.listen(300, () => console.log('Express server running on port 3000'));
+app.listen(3000, () => console.log('Express server running on port 3000'));
