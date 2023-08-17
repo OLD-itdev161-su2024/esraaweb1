a8250eaed2e2a789abca72b3660c23e8803f824b
Esraa <112142189+Esraamurry@users.noreply.github.com>
2023-08-17 15:32:30 -0500

    New activity 5

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
-import config from "config";
+import mongoose from 'mongoose';
+import config from 'config';
 
-//get the connection string
-const db = config.get('mongoURI');
+//Get the connection string
+const db = config.get ('mongoURI');
 
-//connect to MongoDB
+//Connect to MongoDB
 const connectDatabase = async () => {
-    try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true, 
-            //useCreateIndex: true
-        });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.log(error.message);
-
-        //Exit the failure code
-        process.exit(1);
-    }
+try {
+await mongoose.connect (db, {
+useUnifiedTopology: true
+});
+console.log ('Connected to MongoDB');
+} catch (error) {
+console.error (error.message);
+// Exit with failure code
+process.exit(1);
+}
 };
 
-export default connectDatabase;
+export default connectaDtabase;
+
+
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import connectDatabase from './config/db';
-
+import { check, validationResult } from 'express-validator' import cors from 'cors';
+import cors from 'cors';
+import bcrypt from 'bcryptis';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from ' ./models/User';
 
 const app = express();
 
@@ -11,3 +16,68 @@ res.send('http get request sent to root api endpoint')
 );
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
+
+@route POST api/users 
+@desc Register user
+
+app.post(
+'/api/users'
+[
+check('name', 'Please enter your name')
+ .not ()
+ .isEmpty(),
+check('email', 'Please enter a valid email').isEmail(), 
+check(
+'password',
+'Please enter a password with 6 or more characters' 
+).isLength({ min: 6 })
+],
+
+async (req, res) => {
+const errors = validationResult (req);
+if (!errors.isEmpty ()) {
+return res.status (422).json({ errors: errors.array () });
+} else {
+const { name, email, password } = req.body;
+try {
+//Check if user exists
+let user = await User.findOne ({ email: email });
+if (user) {
+return res
+.status (400)
+.json({ errors: [{msg: 'User already exists' }] });
+}
+// Create a new user
+user = new User({
+name: name, 
+email: email, 
+password: password
+});
+
+//Encrypt the password
+const salt = await bcrypt.genSalt (10);
+user.password = await bcrypt.hash (password, salt);
+//Save to the db and return
+await user.save();
+
+const payload = {
+    user: {
+        id: user.id
+    }
+};
+
+jwt.sign(
+    payload,
+    config.get('jwtSecret'),
+    { expiresIn: '10hr' },
+    (err, token) => {
+        if (err) throw err;
+        res.json({ token: token});
+    }
+);
+} catch (error) {
+res.status (500).send ('Server error');
+    }
+   }
+  }
+);
